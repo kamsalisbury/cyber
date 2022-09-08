db3000548b288881d09656fa64ea96cb65122b59
Kam Salisbury <kam.slaisbury.it@icloud.com>
2022-09-08 08:56:17 -0400

    Add pre-req clarification

--- linux-update-redhat-firewall.sh
+++ linux-update-redhat-firewall.sh
@@ -1,4 +1,9 @@
 # A simple bash script to update the Redhat linux host to only accept remote connections from a specific IP.
+# Note that this script uses the zone 'trusted'. The trusted zone should be further restricted. Here is an example.
+# firewall-cmd --zone=trusted --add-source=<IP of desired host or hosts in CIDR>
+# firewall-cmd --zone=trusted --add-service=ssh
+# firewall-cmd --zone=trusted --list-all
+# The result of the example above is that only the specified IP (or IPs) are allowed to connect to the host using ssh.
 (current1=$(firewall-cmd --zone=trusted --list-sources)
 firewall-cmd --zone=trusted --remove-source=$current1)
 (vpn1=$(ping -c1 fqdn | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p')
